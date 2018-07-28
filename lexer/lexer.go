8b0e213941506c04838c6f1d6cc3da0d17979b09
Kamila Hinckel <kamila.hinckel@neoway.com.br>
2018-07-27 22:01:13 -0300

    adding numeric support

--- lexer/lexer.go
+++ lexer/lexer.go
@@ -16,6 +16,9 @@ func New(input string) *Lexer {
 
 func (l *Lexer) NextToken() token.Token {
 		var tok token.Token
+
+		l.skipWhitespace()
+
 		switch l.ch {
 		case '=':
 				tok = newToken(token.ASSIGN, l.ch)
@@ -39,11 +42,15 @@ func (l *Lexer) NextToken() token.Token {
 		default:
 				if isLetter(l.ch) {
 						tok.Literal = l.readIdentifier()
+						tok.Type = token.LookupIdent(tok.Literal)
+						return tok
+				} else if isDigit(l.ch) {
+						tok.Type = token.INT
+						tok.Literal = l.readNumber()
 						return tok
 				} else {
 						tok = newToken(token.ILLEGAL, l.ch)
 				}
-		}
 		}
 		l.readChar()
 		return tok
@@ -73,4 +80,22 @@ func (l *Lexer) readChar() {
 		}
 		l.position = l.readPosition
 		l.readPosition += 1
+}
+
+func (l *Lexer) skipWhitespace() {
+		for l.ch == ' ' || l.ch == '\t' || l.ch == '\n' || l.ch == '\r' {
+				l.readChar()
+		}
+}
+
+func (l *Lexer) readNumber() string {
+		position := l.position
+		for isDigit(l.ch) {
+				l.readChar()
+		}
+		return l.input[position:l.position]
+}
+
+func isDigit(ch byte) bool {
+		return '0' <= ch && ch <= '9'
 }
